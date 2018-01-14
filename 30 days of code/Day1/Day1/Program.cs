958da515a756fe979d561ac008da1acce9e4be06
milosz <miloszloszek@gmail.com>
2018-01-14 13:48:07 +0100

    test commit 2

--- 30 days of code/Day1/Day1/Program.cs
+++ 30 days of code/Day1/Day1/Program.cs
@@ -10,6 +10,11 @@ namespace Day1
     {
         static void Main(string[] args)
         {
+            String inputString;
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputString);
         }
     }
 }
--- 30 days of code/Day2/Day2/Day2/Program.cs
+++ 30 days of code/Day2/Day2/Day2/Program.cs
@@ -10,6 +10,26 @@ namespace Day2
     {
         static void Main(string[] args)
         {
+            
+
+            int i, i2, iResult;
+            double d, d2, dResult;
+            string s, s2, sResult;
+
+            i = 4;
+            d = 4.0;
+            s = "HackerRank ";
+
+            i2 = int.Parse(Console.ReadLine());
+            d2 = Convert.ToDouble(Console.ReadLine());
+            s2 = Console.ReadLine();
+
+            iResult = i + i2;
+            dResult = d + d2;
+            sResult = s + s2;
+
+            Console.Write(iResult + "\n" + String.Format("{0:0.0}", dResult) + "\n" + sResult);
+            Console.ReadKey();
         }
     }
 }
