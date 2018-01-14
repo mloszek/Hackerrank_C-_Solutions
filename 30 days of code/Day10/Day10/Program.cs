62ccae0af1bcdd2ff30e03667f28f77018f6dd6f
milosz <miloszloszek@gmail.com>
2018-01-14 17:25:44 +0100

    next commit

---
+++ 30 days of code/Day10/Day10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    class Program
+    {
+
+        static int factorial(int n) {
+
+            if (n <= 1) {
+                return 1;
+            }
+                return n * factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(factorial(n));
+            Console.ReadKey();
+        }
+    }
+}
--- 30 days of code/Day9/Day9/Program.cs
+++ 30 days of code/Day9/Day9/Program.cs
@@ -10,7 +10,38 @@ namespace Day9
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            int n = Convert.ToInt32(Console.ReadLine());
+            string checkForEntry;
 
+            for (int i = 0; i < n; i++)
+            {
+                string[] dictionaryEntry = Console.ReadLine().Split(' ');
+                dictionary.Add(dictionaryEntry[0], dictionaryEntry[1]);
+            }
+
+            while (true) {
+
+                checkForEntry = Console.ReadLine();
+
+                if (checkForEntry == null)
+                {
+                    break;
+                }
+                else
+                {
+                    if (dictionary.ContainsKey(checkForEntry))
+                    {
+                        Console.WriteLine(checkForEntry + "=" + dictionary[checkForEntry]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                }
+            } 
+
+            Console.ReadKey();
         }
     }
 }
