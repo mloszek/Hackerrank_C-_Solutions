99c06b23a4d80403a0c39d90a928bd5807a0cc20
milosz <miloszloszek@gmail.com>
2018-01-18 21:59:56 +0100

    next commit

---
+++ 30 days of code/Day23/Day23/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    class Solution
+    {
+        
+        static int getHeight(Node root)
+        {
+            var leftHeight = 0;
+            var rightHeight = 0;
+
+            if (root.left == null && root.right == null) {
+                return 0;
+            }
+
+            if (root.left != null) {
+                leftHeight++;
+                leftHeight += getHeight(root.left);
+            }
+
+            if (root.right != null)
+            {
+                rightHeight++;
+                rightHeight += getHeight(root.right);
+            }
+
+            return leftHeight > rightHeight ? leftHeight : rightHeight;
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
+        }
+    }
+}
---
+++ 30 days of code/Day24/Day24/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ 
+     unfinished
+     
+     */
+namespace Day24
+{
+    
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    class Solution
+    {
+        static bool isRoot = true;
+
+        static void levelOrder(Node root)
+        {
+            if (isRoot)
+            {
+                Console.Write("{0} ", root.data);
+                isRoot = false;
+            }
+
+            if (root.left != null) {
+                Console.Write("{0} ", root.left.data);
+            }
+
+            if (root.right != null)
+            {
+                Console.Write("{0} ", root.right.data);
+            }
+
+            if (root.left != null)
+            {
+                levelOrder(root.left);
+            }
+
+            if (root.right != null)
+            {
+                levelOrder(root.right);
+            }
+
+
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
+        }
+    }
+}
