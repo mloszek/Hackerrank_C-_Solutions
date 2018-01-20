2bfeabfccd6979db7fa1399694e6ec00636083d2
milosz <miloszloszek@gmail.com>
2018-01-20 17:26:36 +0100

    next commit

---
+++ 30 days of code/Day26/Day26/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day26
+{
+    class Solution
+    {
+        static string CheckForPrimality(int a)
+        {
+
+            if (a < 2)
+            {
+                return "Not prime";
+            }
+            else if (a == 2)
+            {
+                return "Prime";
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(a); i++)
+                {
+                    if (a % i == 0)
+                    {
+                        return "Not prime";
+                    }
+                }
+                return "Prime";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(CheckForPrimality(Convert.ToInt32(Console.ReadLine())));
+            }
+        }
+    }
+}
